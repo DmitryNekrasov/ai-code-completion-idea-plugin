0f5703f067f952c45de4f92c66e70d63c8e83373
Dmitry Nekrasov <scannorone@gmail.com>
2024-07-01 20:02:53 +0400

    Add test completion for System.out.println(sum);

--- src/main/kotlin/com/aicc/aicodecompletionideaplugin/AICCInlineCompletionProvider.kt
+++ src/main/kotlin/com/aicc/aicodecompletionideaplugin/AICCInlineCompletionProvider.kt
@@ -7,12 +7,32 @@ class AICCInlineCompletionProvider : InlineCompletionProvider {
     override val id = InlineCompletionProviderID("AICCInlineCompletionProvider")
 
     override suspend fun getSuggestion(request: InlineCompletionRequest): InlineCompletionSuggestion {
+        val offset = request.startOffset
+        val text = request.document.text.substring(0..offset)
+        val lastLine = text.lines().last()
+
+        var suggestion = ""
+        for (i in 0..EXAMPLE_STRING.lastIndex) {
+            val prefix = EXAMPLE_STRING.substring(0..i)
+            if (lastLine.endsWith(prefix)) {
+                val suffix = EXAMPLE_STRING.substring(i + 1)
+                println("prefix = $prefix")
+                println("suffix = $suffix")
+                suggestion = suffix
+                break
+            }
+        }
+
         return InlineCompletionSuggestion.withFlow {
-            emit(InlineCompletionGrayTextElement("Hello, World!"))
+            emit(InlineCompletionGrayTextElement(suggestion))
         }
     }
 
     override fun isEnabled(event: InlineCompletionEvent): Boolean {
         return event is InlineCompletionEvent.DocumentChange || event is InlineCompletionEvent.DirectCall
+    }
+
+    companion object {
+        const val EXAMPLE_STRING = "System.out.println(sum);"
     }
 }
