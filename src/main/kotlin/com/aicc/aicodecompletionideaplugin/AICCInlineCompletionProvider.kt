f653a49d2b26d745ca8f2febf5c9c134c8416ec1
Dmitry Nekrasov <scannorone@gmail.com>
2024-07-01 19:25:17 +0400

    Print Hello World in CompletionGrayTextElement

---
+++ src/main/kotlin/com/aicc/aicodecompletionideaplugin/AICCInlineCompletionProvider.kt
@@ -0,0 +1,18 @@
+package com.aicc.aicodecompletionideaplugin
+
+import com.intellij.codeInsight.inline.completion.*
+import com.intellij.codeInsight.inline.completion.elements.InlineCompletionGrayTextElement
+
+class AICCInlineCompletionProvider : InlineCompletionProvider {
+    override val id = InlineCompletionProviderID("AICCInlineCompletionProvider")
+
+    override suspend fun getSuggestion(request: InlineCompletionRequest): InlineCompletionSuggestion {
+        return InlineCompletionSuggestion.withFlow {
+            emit(InlineCompletionGrayTextElement("Hello, World!"))
+        }
+    }
+
+    override fun isEnabled(event: InlineCompletionEvent): Boolean {
+        return true
+    }
+}
