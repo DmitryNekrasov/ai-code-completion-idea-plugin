463e0a8f91c3631982310ffdf7d73a4f3dfae038
Dmitry Nekrasov <scannorone@gmail.com>
2024-07-01 19:03:28 +0400

    Increased minimum supported IDEA version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2023.2.6")
+  version.set("2023.3.6")
   type.set("IC") // Target IDE Platform
 
   plugins.set(listOf(/* Plugin Dependencies */))
@@ -31,7 +31,7 @@ tasks {
   }
 
   patchPluginXml {
-    sinceBuild.set("232")
+    sinceBuild.set("233")
     untilBuild.set("242.*")
   }
 
