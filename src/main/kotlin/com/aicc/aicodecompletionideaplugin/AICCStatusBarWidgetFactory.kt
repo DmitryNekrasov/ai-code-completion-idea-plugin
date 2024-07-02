fc883f07a171de2910d9186868437cde1f8a3a5d
Dmitry Nekrasov <scannorone@gmail.com>
2024-07-02 18:59:17 +0400

    Add documentation for AICCStatusBarWidgetFactory

--- src/main/kotlin/com/aicc/aicodecompletionideaplugin/AICCStatusBarWidgetFactory.kt
+++ src/main/kotlin/com/aicc/aicodecompletionideaplugin/AICCStatusBarWidgetFactory.kt
@@ -8,18 +8,42 @@ import com.intellij.openapi.wm.WindowManager
 import com.intellij.openapi.wm.impl.status.EditorBasedWidget
 import java.awt.Component
 
+/**
+ * This class is a factory for creating instances of `AICCStatusBarWidget`.
+ * It implements the `StatusBarWidgetFactory` interface.
+ */
 class AICCStatusBarWidgetFactory : StatusBarWidgetFactory {
+    /**
+     * Returns the unique identifier for this widget factory.
+     */
     override fun getId() = ID
 
+    /**
+     * Returns the display name for this widget factory.
+     */
     override fun getDisplayName() = ID
 
+    /**
+     * Creates a new instance of `AICCStatusBarWidget` for the given project.
+     */
     override fun createWidget(project: Project) = AICCStatusBarWidget(project)
 
+    /**
+     * Checks if this widget is available for the given project.
+     * Always returns true.
+     */
     override fun isAvailable(project: Project) = true
 
+    /**
+     * Checks if this widget can be enabled on the given status bar.
+     * Always returns true.
+     */
     override fun canBeEnabledOn(statusBar: com.intellij.openapi.wm.StatusBar) = true
 
     companion object {
+        /**
+         * The unique identifier for this widget factory.
+         */
         const val ID = "AICCStatusBarWidgetFactory"
     }
 }
