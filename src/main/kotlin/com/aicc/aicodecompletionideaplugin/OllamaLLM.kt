ac88fb73b0c86d6d7c2b428c2e31c82a6e9e975d
Dmitry Nekrasov <scannorone@gmail.com>
2024-07-04 18:03:04 +0400

    Add retry logic with RETRY_COUNT in OllamaLLM for handling timeouts

--- src/main/kotlin/com/aicc/aicodecompletionideaplugin/OllamaLLM.kt
+++ src/main/kotlin/com/aicc/aicodecompletionideaplugin/OllamaLLM.kt
@@ -3,21 +3,31 @@ package com.aicc.aicodecompletionideaplugin
 import io.github.amithkoujalgi.ollama4j.core.OllamaAPI
 import io.github.amithkoujalgi.ollama4j.core.utils.Options
 import io.github.amithkoujalgi.ollama4j.core.utils.OptionsBuilder
+import java.net.http.HttpTimeoutException
 
 object OllamaLLM : LLM {
     override fun call(prefix: String, suffix: String): String? {
-        return if (isEnable) {
+        if (isEnable) {
             AICCStatusBarWidgetManager.updateStatus("OK")
-            api.generate(MODEL, "<PRE> $prefix <SUF>$suffix <MID>", options).response.let {
-                if (it.endsWith(END)) it.substring(0, it.length - END.length).trimEnd() else it
+            for (i in 0..<RETRY_COUNT) {
+                val suggestion = try {
+                    OllamaAPI(HOST).apply {
+                        setRequestTimeoutSeconds(4)
+                    }.generate(MODEL, "<PRE> $prefix <SUF>$suffix <MID>", options).response.let {
+                        if (it.endsWith(END)) it.substring(0, it.length - END.length).trim(' ', '\t', '\n') else it
+                    }
+                } catch (e: HttpTimeoutException) {
+                    continue
+                }
+                if (suggestion.isNotBlank()) {
+                    return suggestion
+                }
             }
         } else {
             AICCStatusBarWidgetManager.updateStatus("Ollama server is not reachable")
-            null
         }
+        return null
     }
-
-    private val api: OllamaAPI by lazy { OllamaAPI(HOST) }
 
     private val options: Options by lazy {
         OptionsBuilder()
@@ -28,7 +38,7 @@ object OllamaLLM : LLM {
     private val isEnable: Boolean
         get() {
             return try {
-                api.ping()
+                OllamaAPI(HOST).ping()
             } catch (e: Exception) {
                 false
             }
@@ -48,4 +58,11 @@ object OllamaLLM : LLM {
      * The end of text marker used in the responses from the Ollama API.
      */
     private const val END = "<EOT>"
+
+    /**
+     * The number of attempts to retry the API call in case of a timeout.
+     * This constant defines how many times the system should attempt to call the Ollama API
+     * before giving up, in the event of a network timeout or other transient errors.
+     */
+    private const val RETRY_COUNT = 4
 }
