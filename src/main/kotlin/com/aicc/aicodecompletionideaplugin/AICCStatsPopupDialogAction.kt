ddfb2cc0e58c48a25208bd83d23f8d7ed495cd46
Dmitry Nekrasov <scannorone@gmail.com>
2024-07-04 17:48:31 +0400

    Add statistic output

--- src/main/kotlin/com/aicc/aicodecompletionideaplugin/AICCStatsPopupDialogAction.kt
+++ src/main/kotlin/com/aicc/aicodecompletionideaplugin/AICCStatsPopupDialogAction.kt
@@ -6,7 +6,10 @@ import com.intellij.openapi.ui.Messages
 
 class AICCStatsPopupDialogAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
-        val message = "TODO: Stats will be shown here"
+        val message = "Total completions: ${AICCStatistic.totalCompletionsNumber}\n" +
+                "Successful completions: ${AICCStatistic.successfulCompletionsNumber}\n" +
+                "User engagement: ${AICCStatistic.userEngagement}%\n" +
+                "Average completion time: %.2f s".format(AICCStatistic.averageCompletionTime)
         Messages.showMessageDialog(
             event.project,
             message,
