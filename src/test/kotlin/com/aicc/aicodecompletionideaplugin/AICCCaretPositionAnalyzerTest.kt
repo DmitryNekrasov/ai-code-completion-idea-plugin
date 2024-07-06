78c6e89f1c1870eab4d23abd8cb09e914454c862
Dmitry Nekrasov <scannorone@gmail.com>
2024-07-06 16:37:30 +0400

    Add tests for CaretPositionAnalyzer

---
+++ src/test/kotlin/com/aicc/aicodecompletionideaplugin/AICCCaretPositionAnalyzerTest.kt
@@ -0,0 +1,50 @@
+package com.aicc.aicodecompletionideaplugin
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class AICCCaretPositionAnalyzerTest {
+    @Test
+    fun `test inside literal`() {
+        assertTrue("int x = 123;".shouldBeSkippedOnPosition(9))
+        assertTrue("String str = \"Hello, world!\";".shouldBeSkippedOnPosition(16))
+    }
+
+    @Test
+    fun `test inside identifier or keyword`() {
+        assertTrue("int myAmazingVariable = 123;".shouldBeSkippedOnPosition(7))
+        assertTrue("System.out.println();".shouldBeSkippedOnPosition(15))
+        assertTrue("boolean f = true;".shouldBeSkippedOnPosition(4))
+    }
+
+    @Test
+    fun `test after semicolon`() {
+        assertTrue("int x = 123;".shouldBeSkippedOnPosition(11))
+    }
+
+    @Test
+    fun `test after left brace`() {
+        assertTrue("if (condition) {".shouldBeSkippedOnPosition(15))
+    }
+
+    @Test
+    fun `test after right brace`() {
+        assertTrue("if (condition) {}".shouldBeSkippedOnPosition(16))
+    }
+
+    @Test
+    fun `test before left parenthesis`() {
+        assertTrue("System.out.println();".shouldBeSkippedOnPosition(17))
+    }
+
+    @Test
+    fun `test after right parenthesis`() {
+        assertTrue("static void myMethod()".shouldBeSkippedOnPosition(21))
+    }
+
+    @Test
+    fun `test outside any specific context`() {
+        assertFalse("int x = 123;".shouldBeSkippedOnPosition(4))
+    }
+}
