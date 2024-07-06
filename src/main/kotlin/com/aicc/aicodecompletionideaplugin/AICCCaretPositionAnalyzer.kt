b649c7befef9dc3db737b90f46cc447cd74dcd4c
Dmitry Nekrasov <scannorone@gmail.com>
2024-07-06 16:20:40 +0400

    Add documentation for CaretPositionAnalyzer

--- src/main/kotlin/com/aicc/aicodecompletionideaplugin/AICCCaretPositionAnalyzer.kt
+++ src/main/kotlin/com/aicc/aicodecompletionideaplugin/AICCCaretPositionAnalyzer.kt
@@ -31,6 +31,14 @@
  */
 package com.aicc.aicodecompletionideaplugin
 
+/**
+ * Determines if the caret position, represented by `offset` in the given `String`, should be skipped for code
+ * completion suggestions. This decision is based on the syntactical context immediately surrounding the caret,
+ * such as specific characters or structures that typically do not require suggestions.
+ *
+ * @param offset The current position of the caret within the text.
+ * @return `true` if the caret position should be skipped for suggestions, `false` otherwise.
+ */
 fun String.shouldBeSkippedOnPosition(offset: Int) = checkElementUnderCaret(this, offset) {
     afterSemicolon()
             || afterLBrace()
@@ -41,30 +49,69 @@ fun String.shouldBeSkippedOnPosition(offset: Int) = checkElementUnderCaret(this,
 
 }
 
+/**
+ * Checks if the caret is immediately after a semicolon.
+ *
+ * @return `true` if the caret is after a semicolon, `false` otherwise.
+ */
 private fun Pair<Char, Char>.afterSemicolon(): Boolean {
     return first == ';'
 }
 
+/**
+ * Checks if the caret is immediately after a left brace.
+ *
+ * @return `true` if the caret is after a left brace, `false` otherwise.
+ */
 private fun Pair<Char, Char>.afterLBrace(): Boolean {
     return first == '{'
 }
 
+/**
+ * Checks if the caret is immediately after a right brace.
+ *
+ * @return `true` if the caret is after a right brace, `false` otherwise.
+ */
 private fun Pair<Char, Char>.afterRBrace(): Boolean {
     return first == '}'
 }
 
+/**
+ * Checks if the caret is immediately before a left parenthesis.
+ *
+ * @return `true` if the caret is before a left parenthesis, `false` otherwise.
+ */
 private fun Pair<Char, Char>.beforeLParenthesis(): Boolean {
     return second == '('
 }
 
+/**
+ * Checks if the caret is immediately after a right parenthesis.
+ *
+ * @return `true` if the caret is after a right parenthesis, `false` otherwise.
+ */
 private fun Pair<Char, Char>.afterRParenthesis(): Boolean {
     return first == ')'
 }
 
+/**
+ * Checks if the caret is inside an identifier, determined by both characters around the caret being digit or letter.
+ *
+ * @return `true` if the caret is inside an identifier, `false` otherwise.
+ */
 private fun Pair<Char, Char>.insideIdentifier(): Boolean {
     return first.isLetterOrDigit() && second.isLetterOrDigit()
 }
 
+/**
+ * Evaluates the text at the caret position against a set of criteria to determine if the position should be skipped.
+ * This function uses a pair of characters surrounding the caret to make this determination.
+ *
+ * @param text The entire text of the document.
+ * @param offset The current position of the caret within the text.
+ * @param satisfySkippedCriteria A lambda function that defines the criteria for skipping the caret position.
+ * @return `true` if the caret position satisfies the criteria for being skipped, `false` otherwise.
+ */
 private fun checkElementUnderCaret(
     text: String,
     offset: Int,
