ad8bcc42263bc4ec61df633bdaf83c5711e7eec0
Dmitry Nekrasov <scannorone@gmail.com>
2024-07-06 16:16:20 +0400

    Add detail description for CaretPositionAnalyzer

--- src/main/kotlin/com/aicc/aicodecompletionideaplugin/AICCCaretPositionAnalyzer.kt
+++ src/main/kotlin/com/aicc/aicodecompletionideaplugin/AICCCaretPositionAnalyzer.kt
@@ -1,3 +1,34 @@
+/**
+ * Provides functionality to analyze the caret position within the code editor and determine if the current position
+ * should be skipped for code completion suggestions. This determination is based on the syntactical context of the
+ * caret position.
+ *
+ * It covers the next cases:
+ *
+ * 1. If caret is inside literal:
+ *      int x = 12|3;
+ *      String str = "Hell|o, world!";
+ *
+ * 2. If caret is inside identifier or keyword:
+ *      int myAm|azingVariable = 123;
+ *      System.out.prin|tln();
+ *      bool|ean f = true;
+ *
+ * 3. If caret is after semicolon:
+ *      int x = 123;|
+ *
+ * 4. If caret is after left brace:
+ *      if (condition) {|
+ *
+ * 5. If caret is after right brace:
+ *      if (condition) {}|
+ *
+ * 6. If caret is before left parenthesis:
+ *      System.out.println|();
+ *
+ * 7. If caret is after right parenthesis:
+ *     static void myMethod()|
+ */
 package com.aicc.aicodecompletionideaplugin
 
 fun String.shouldBeSkippedOnPosition(offset: Int) = checkElementUnderCaret(this, offset) {
