d39fec7dedb32920e6f47e8842d9c26369698ff1
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2026-02-09 13:50:16 +0400

    Downgrade Kotlin to 2.1.10 in build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("jvm") version "2.3.0"
+    kotlin("jvm") version "2.1.10"
     id("java")
     id("org.jetbrains.intellij.platform")
 }
